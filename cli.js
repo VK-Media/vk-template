fd394b70b4a9ed02ddc952f60fbf2442fc6732d1
Robin Louis van Komen <robin@vkmedia.dk>
2019-07-20 14:04:36 +0200

    add logic to run npm install as part of creation

--- cli.js
+++ cli.js
@@ -15,10 +15,7 @@ clear();
 const run = async () => {
 	const currentDirectory = process.cwd();
 	const answer = await inquirer.askProjectDetails();
-	const cloningStatus = ora({
-		text: "Cloning project...",
-		spinner: "toggle"
-	}).start();
+	const cloningStatus = ora("Cloning project...").start();
 
 	try {
 		const newDirectory = `${currentDirectory}/${answer.name}`;
@@ -30,9 +27,7 @@ const run = async () => {
 		}
 
 		if (fs.existsSync(newDirectory)) {
-			cloningStatus.fail(
-				"A folder with the specified name already exists."
-			);
+			cloningStatus.fail("A folder with the specified name already exists.");
 			process.exit(1);
 		}
 
@@ -44,20 +39,18 @@ const run = async () => {
 		shell.exec(`git clone ${gitUrl} ${answer.name}`, { silent: true });
 		cloningStatus.succeed("Successfully cloned template");
 
-		const gitStatus = ora({
-			text: "Initializing git...",
-			spinner: "toggle"
-		}).start();
+		const gitStatus = ora("Initializing git...").start();
 
 		rimraf.sync(`${newDirectory}/.git`);
 		shell.exec(`cd ${newDirectory} && git init`, { silent: true });
 		gitStatus.succeed("Initialized git repository");
 
+		npmInstallStatus = ora("Installing dependencies...").start();
+		shell.exec("npm i");
+		npmInstallStatus.succeed("Installed dependencies.");
+
 		if (shell.which("code") && answer.vsc) {
-			const vscStatus = ora({
-				text: "Opening new project in Visual Studio Code...",
-				spinner: "toggle"
-			}).start();
+			const vscStatus = ora("Opening new project in Visual Studio Code...").start();
 
 			shell.exec(`code ${newDirectory}`);
 			vscStatus.succeed("Opened new project in Visual Studio Code.");
@@ -65,9 +58,7 @@ const run = async () => {
 
 		console.log("\n");
 		console.log(
-			chalk.green(
-				"The project has been successfully created, run the following commands to get started:"
-			)
+			chalk.green("The project has been successfully created, run the following commands to get started:")
 		);
 		console.log(chalk.blue("cd "), answer.name);
 		console.log(chalk.blue("npm "), "start");
