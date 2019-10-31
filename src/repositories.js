2bb8033193c381905cd2bdd76a152542a02b0777
Robin Louis van Komen <robin@vkmedia.dk>
2019-10-31 07:24:08 +0100

    add 2 new templates

--- src/repositories.js
+++ src/repositories.js
@@ -1,10 +1,14 @@
 module.exports = {
-  getRepositories: () => {
-    return {
-      "react-redux-typescript":
-        "https://github.com/VK-Media/react-redux-typescript-template.git",
-      "express-typescript-api":
-        "https://github.com/VK-Media/express-typescript-api-template.git"
-    };
-  }
+    getRepositories: () => {
+        return {
+            "react-redux-typescript":
+                "https://github.com/VK-Media/react-redux-typescript-template.git",
+            "express-typescript-api":
+                "https://github.com/VK-Media/express-typescript-api-template.git",
+            "graphql-apollo-server":
+                "https://github.com/VK-Media/graphql-apollo-server-template.git",
+            "react-graphql-apollo-typescript":
+                "https://github.com/VK-Media/react-graphql-apollo-typescript.git"
+        };
+    }
 };
